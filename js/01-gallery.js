452485b90b3e8c0d8c256d4c6a3fff77d8699767
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-02 21:21:44 +0300

    done task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const text = '4'
-console.log(galleryItems, `<div>asdf ${text}</div>`);
+
+const galleryRef = document.querySelector('.gallery');
+
+const getItemMarkup = ({preview, original, description}) => `
+  <div class="gallery__item">
+    <a class="gallery__link">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        width="340"
+        loading="lazy"
+        data-big-url="${original}"
+        alt="${description}"/>
+    </a>
+  </div>
+`
+const getImagesMarkup = (images) => images.map(it => getItemMarkup(it)).join('')
+
+galleryRef.innerHTML = getImagesMarkup(galleryItems);
+galleryRef.addEventListener('click', onClickGallery)
+
+function onClickGallery(e) {
+  const link = e.target.closest('.gallery__link');
+  const img = link?.querySelector('.gallery__image')
+
+  if (!(link && img)) return;
+  const {alt: description, dataset: {bigUrl: url}} = img
+
+  showBigImg({url, description})
+}
+
+function showBigImg({url, description}) {
+  const instance = window.basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${url}"
+      width="1280"
+      alt="${description}"/>
+  `)
+
+  instance.show()
+}
