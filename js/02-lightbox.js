c0e55a77c9f174135b01d8a8e0e2b020c9fadff3
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-02 22:26:54 +0300

    done task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const getItemMarkup = ({preview, original, description}) => `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+`
+const getImagesMarkup = (images) => images.map(it => getItemMarkup(it)).join('')
+
+galleryRef.innerHTML = getImagesMarkup(galleryItems);
+galleryRef.addEventListener('click', onClickGallery)
+
+new window.SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt'
+});
+
+function onClickGallery(e) {
+  e.preventDefault();
+}
+
